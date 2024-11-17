c407ed13d8dbd07c5a9a536633c69759262551fa
Paul Adrian Gernale <51394339+bananahabit@users.noreply.github.com>
2024-11-17 22:23:37 +0800

    Uploaded the code for SimpleBanking.sol

---
+++ Functions And Error ETH Avax/SimpleBanking.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract SimpleBanking {
+
+    uint public CurrentBalance = 0; 
+
+    function DepositFund (uint _val) public{
+        require (_val > 0, "Please insert more than 0 to your account."); 
+        uint PreviousBalance = CurrentBalance;
+        CurrentBalance += _val;
+
+        assert (CurrentBalance > PreviousBalance); 
+    }
+
+    function WindrawFund (uint _val) public{
+        require (_val > 0, "Withdraw an amount greater than 0."); 
+        require (_val <= CurrentBalance, "You do not have enough balance to withdraw that amount."); 
+        uint PreviousBalance = CurrentBalance; 
+        CurrentBalance -= _val; 
+        
+        assert (CurrentBalance < PreviousBalance || CurrentBalance == 0); 
+    } 
+}
+
+
+
