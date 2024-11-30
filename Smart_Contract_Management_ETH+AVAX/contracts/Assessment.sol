6ddaa3e2aff2bebebfef30d58cef9d446090b46c
Paul Adrian Gernale <51394339+bananahabit@users.noreply.github.com>
2024-11-30 22:25:51 +0800

    Update Assessment.sol with the required function changes

--- Smart_Contract_Management_ETH+AVAX/contracts/Assessment.sol
+++ Smart_Contract_Management_ETH+AVAX/contracts/Assessment.sol
@@ -6,12 +6,14 @@ pragma solidity ^0.8.9;
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    uint256 public shawarmaTotal;
+
     //
     bool public isUnlocked;
 
-
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
+    
+    event AddMoney(uint256 amount);
+    event BuyShawarma(uint256 amount); 
     event UnlockedAccount(); 
     event LockedAccount(); 
 
@@ -49,28 +51,33 @@ contract Assessment {
         return balance;
     }
 
-    function deposit(uint256 _amount) public payable onlyOwner{
+    function getShawarma() public view returns (uint256){
+        return shawarmaTotal; 
+    }
+
+    function addMoney(uint256 _amount) public payable onlyOwner{
         uint _previousBalance = balance;
 
         // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
 
-        // perform transaction
+        // adds the value inputed to the current balance
         balance += _amount;
 
-        // assert transaction completed successfully
+        // checks whether the value of the balance increased
         assert(balance == _previousBalance + _amount);
 
         // emit the event
-        emit Deposit(_amount);
+        emit AddMoney(_amount);
     }
 
     // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
-    function withdraw(uint256 _withdrawAmount) public onlyOwner{
+    function buyShawarma (uint256 _withdrawAmount) public onlyOwner{
         require(msg.sender == owner, "You are not the owner of this account");
         uint _previousBalance = balance;
+
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
@@ -78,13 +85,16 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
+        // subtract the given amount
         balance -= _withdrawAmount;
 
-        // assert the balance is correct
+        // add 1 to the total shawarma 
+        shawarmaTotal += 1; 
+
+        // checks if the current balance was subtracted based on the given amount 
         assert(balance == (_previousBalance - _withdrawAmount));
 
         // emit the event
-        emit Withdraw(_withdrawAmount);
+        emit BuyShawarma(_withdrawAmount);
     }
 }
